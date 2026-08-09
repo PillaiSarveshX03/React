2839c8bb1b1cd8d807d7c4b2d61b249f4665a164
Sarvesh Pillai <pillaisarvesh03@gmail.com>
2026-08-09 12:23:00 +0000

    CSS mods, Inline, External

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,22 @@
+
+function Button(){
+
+    const styles = {
+        backgroundColor: "hsl(248, 82%, 55%)",
+        maxWidth: "100px",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+  
+    }
+
+
+    return(
+       <button style={styles}>Button</button>
+    );
+
+
+
+    
+} export default Button
